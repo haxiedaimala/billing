50e0b3050ca5bed62f6364a7182d423af3d73ca7
myself <1907312518@qq.com>
2023-02-24 22:33:02 +0800

    填充 NotFound.vue 内容：修饰 404 页面

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,9 +1,4 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <div>404</div>
+  <div>当前页面不存在，请检查网址是否正确</div>
+  <router-link to="/">返回首页</router-link>
 </template>
-
-<style lang="scss" scoped>
-</style>
