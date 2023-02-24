5acf35dee694b21ba1eb383a21cb8149278e309c
myself <1907312518@qq.com>
2023-02-25 00:13:04 +0800

    使用 svg-sprite-loader 插件引入 svg

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,11 +1,33 @@
 <script setup lang="ts">
+import x from '@/assets/icons/money.svg';
+import y from '@/assets/icons/label.svg';
+import z from '@/assets/icons/statistics.svg';
+
+console.log(x);
+console.log(y);
+console.log(z);
 </script>
 
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
-    <router-link to="/label">标签</router-link>
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/money">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#money"></use>
+      </svg>
+      记账
+    </router-link>
+    <router-link to="/label">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#label"></use>
+      </svg>
+      标签
+    </router-link>
+    <router-link to="/statistics">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#statistics"></use>
+      </svg>
+      统计
+    </router-link>
   </div>
 </template>
 
