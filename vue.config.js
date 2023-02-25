7ab369659d96c602e6ad5e89450fad2ba028da8b
myself <1907312518@qq.com>
2023-02-25 19:50:28 +0800

    按照文档修改成功使用svgo-loader 去除自带 fill

--- vue.config.js
+++ vue.config.js
@@ -15,7 +15,7 @@ module.exports = defineConfig({
       .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()
       .use('svgo-loader').loader('svgo-loader')
       .tap(options => ({
-        ...options, plugins: [{removeAttribute: {attrs:'fill'}}]
+        ...options, plugins: [{name: 'removeAttrs', params: {attrs: '(fill|stroke)'}}]
       })).end()
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
     config.module.rule('svg').exclude.add(dir) //其他 svg loader 排除 icons 目录
