b3da9b95ff5d12eb03a27f0848539c6216462747
myself <1907312518@qq.com>
2023-02-25 21:15:03 +0800

    Money 样式 CSS3

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -98,4 +98,32 @@ $border-radius: $h/2;
     background: transparent;
   }
 }
+
+.types {
+  display: flex;
+  align-items: center;
+  background: #c4c4c4;
+  font-size: 24px;
+
+  li {
+    flex: 1;
+    padding: 15px 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+
+    &.selected {
+      &::after {
+        content: '';
+        width: 100%;
+        height: 4px;
+        background: #333;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+      }
+    }
+  }
+}
 </style>
