94ebaf5a760b43f2c0aecc564223314b175ca4e3
myself <1907312518@qq.com>
2023-02-26 17:44:09 +0800

    版本迁移

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -5,6 +5,31 @@ import Types from '@/components/money/Types.vue';
 import NumberPad from '@/components/money/NumberPad.vue';
 import {computed, ref} from 'vue';
 
+/*
+* 数据版本迁移
+*
+* 检查现有的版本
+* 所有的版本都需要从前到后每一个版本迁移进行 0.0.1=>0.0.2  0.0.2=>0.0.3 0.0.3=>0.0.4,
+* 这样的话，比如版本3.1.1版本迁移到 3.1.2， 只需要从比较3.1.1 3.1.2两个版本的不同，进行迁移即可，因为3.1.1也是从前往后逐步迁移的
+* */
+const version = localStorage.getItem('version');
+const recordList = computed<Record[]>(() => JSON.parse(localStorage.getItem('recordList') || '[]'));
+if (version === '0.0.1') {
+  //数据库升级 ,数据迁移
+  recordList.value.forEach(record => {
+    record.createAt = new Date(0);
+  });
+  //保存数据
+  localStorage.setItem('recordList', JSON.stringify(recordList.value));
+} else if (version === '0.0.2') {
+  //数据库升级 ,数据迁移
+  // ...
+  //保存数据
+  // ...
+}
+localStorage.setItem('version', '0.0.3');
+
+
 interface Record {
   tags: string[],
   note: string,
@@ -27,7 +52,7 @@ const record = computed<Record>(() => {
     createAt: undefined
   };
 });
-const recordList = computed<Record[]>(() => JSON.parse(localStorage.getItem('recordList') || '[]'));
+// const recordList = computed<Record[]>(() => JSON.parse(localStorage.getItem('recordList') || '[]'));
 const onSaveRecord = () => {
   record.value.createAt = new Date();
   recordList.value.push(record.value);
