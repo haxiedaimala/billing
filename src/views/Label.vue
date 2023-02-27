400f9157b6308ad21a3a6ff9e923081209df9e1f
myself <1907312518@qq.com>
2023-02-27 12:03:57 +0800

    展示和创建 Label.vue

--- src/views/Label.vue
+++ src/views/Label.vue
@@ -2,8 +2,69 @@
 </script>
 
 <template>
-  <Layout>Label</Layout>
+  <Layout>
+    <ol class="tags">
+      <li>
+        <span>衣</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>食</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>住</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>行</span>
+        <Icon name="right"/>
+      </li>
+    </ol>
+    <div class="createTag-wrapper">
+      <button class="createTag">新建标签</button>
+    </div>
+  </Layout>
 </template>
 
 <style lang="scss" scoped>
+.tags {
+  background: #fff;
+  font-size: 16px;
+  padding-left: 1em;
+
+  > li {
+    min-height: 3em;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #e6e6e6;
+    cursor: pointer;
+
+    .icon {
+      width: 18px;
+      height: 18px;
+      color: #666;
+      margin-right: 1em;
+    }
+  }
+}
+
+.createTag {
+  background: #767676;
+  color: #fff;
+  border-radius: 4px;
+  border: none;
+  display: flex;
+  align-items: center;
+  padding: 0.6em 1em;
+  cursor: pointer;
+
+  &-wrapper {
+    display: flex;
+    justify-content: center;
+    padding: 16px;
+    margin-top: 44-16px;
+  }
+}
 </style>
