c4f11b2b2312d19131a0f5d2c75e38ca5af72c97
myself <1907312518@qq.com>
2023-03-01 00:23:10 +0800

    抽离 Tabs.vue,使用在 Statistics.vue 里面

---
+++ src/components/Tabs.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+type DateSource = {
+  text: string,
+  value: string
+}
+const props = defineProps<{
+  dataSource: DateSource[],
+  modelValue: string,
+  classClearfix: string
+}>();
+const emits = defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>();
+const toggle = (data: string) => {
+  emits('update:modelValue', data);
+};
+const classList = (item: DateSource) => {
+  return {selected: props.modelValue === item.value, [`${props.classClearfix}-item`]: props.classClearfix};
+};
+</script>
+
+<template>
+  <ul class="tabs">
+    <li v-for="item in dataSource"
+        :key="item.value"
+        @click="toggle(item.value)"
+        :class="classList(item)"
+    >
+      {{ item.text }}
+    </li>
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+.tabs {
+  display: flex;
+  align-items: center;
+  background: #c4c4c4;
+  font-size: 24px;
+
+  li {
+    flex: 1;
+    padding: 15px 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+
+    &.selected {
+      &::after {
+        content: '';
+        width: 100%;
+        height: 4px;
+        background: #333;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+      }
+    }
+  }
+}
+</style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,9 +1,39 @@
 <script setup lang="ts">
+import Tabs from '@/components/Tabs.vue';
+import {ref} from 'vue';
+
+
+const intervalList = [
+  {text: '按天', value: 'day'},
+  {text: '按周', value: 'week'},
+  {text: '按月', value: 'month'},
+];
+const typeList = [
+  {text: '支出', value: '-'},
+  {text: '收入', value: '+'}
+];
+const interval = ref('day');
+const type = ref('-');
 </script>
 
 <template>
-  <Layout>Statistics</Layout>
+  <Layout>
+    <Tabs :data-source="typeList" v-model="type" class-clearfix="type"/>
+    <Tabs :data-source="intervalList" v-model="interval" class-clearfix="statistics"/>
+  </Layout>
 </template>
 
 <style lang="scss" scoped>
+:deep(.type-item) {
+  background-color: #fff;
+  border: none;
+
+  &.selected {
+    background-color: #c4c4c4;
+
+    &::after {
+      display: none;
+    }
+  }
+}
 </style>
