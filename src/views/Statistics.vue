d650507d6256537730386817ee27f082bba8adbb
myself <1907312518@qq.com>
2023-03-01 16:15:06 +0800

    优化 1

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -15,10 +15,8 @@ const beautify = (date: string) => {
       return '昨天';
     } else if (dayjs(date).isSame(dayjs().subtract(2, 'day'), 'day')) {
       return '前天';
-    } else if (dayjs(date).isSame(dayjs(), 'year')) {
-      return dayjs(date).format('M月D日');
     } else {
-      return date;
+      return dayjs(date).format('M月D日');
     }
   }
   if (interval.value === 'month') {
@@ -43,23 +41,45 @@ function clone<T>(data: T): T {
 
 const reslut = computed(() => {
   if (recordList.value.length === 0) return;
-  let groupList: { title: string, total?: number, items: RecordItem[] }[] = [];
+  // let groupList: { title: string, total?: number, items: RecordItem[] }[] = [];
+  let groupList: { year: string, list: { title: string, total?: number, items: RecordItem[] }[] }[] = [];
   const newList = clone(recordList.value)
       .filter(el => el.type === type.value)
       .sort((b, a) => dayjs(a.createAt).valueOf() - dayjs(b.createAt).valueOf());
-  groupList[0] = {title: dayjs(newList[0].createAt).format('YYYY-MM-DD'), total: 0, items: [newList[0]]};
+  // groupList[0] = {title: dayjs(newList[0].createAt).format('YYYY-MM-DD'), total: 0, items: [newList[0]]};
+  groupList[0] = {
+    year: dayjs(newList[0].createAt).format('YYYY'),
+    list: [{
+      title: dayjs(newList[0].createAt).format('YYYY-MM-DD'),
+      total: 0,
+      items: [newList[0]]
+    }]
+  };
   for (let i = 1; i < newList.length; i++) {
     const current = newList[i];
     const last = groupList[groupList.length - 1];
-    if (dayjs(last.title).isSame(dayjs(current.createAt), `${interval.value}` as OpUnitType)) {
-      last.items.push(current);
+    if (last.year === dayjs(current.createAt).format('YYYY')) {
+      for (let j = 0; j < last.list.length; j++) {
+        const lastList = last.list[last.list.length - 1];
+        if (dayjs(lastList.title).isSame(dayjs(current.createAt), `${interval.value}` as OpUnitType)) {
+          lastList.items.push(current);
+        } else {
+          last.list.push({title: dayjs(current.createAt).format('YYYY-MM-DD'), total: 0, items: [current]});
+        }
+      }
     } else {
-      groupList.push({title: dayjs(current.createAt).format('YYYY-MM-DD'), total: 0, items: [current]});
+      groupList.push({
+        year: dayjs(current.createAt).format('YYYY'),
+        list: [{title: dayjs(current.createAt).format('YYYY-MM-DD'), total: 0, items: [current]}]
+      });
     }
   }
   groupList.forEach(group => {
-    group.total = group.items.reduce((sum, item) => sum + item.account, 0);
+    group.list.forEach((item => {
+      item.total = item.items.reduce((sum, item) => sum + item.account, 0);
+    }));
   });
+  console.log(groupList);
   return groupList;
 });
 
@@ -75,15 +95,20 @@ const tagString = (tags: Tag[]) => {
     <Tabs :data-source="intervalList" v-model="interval" class-clearfix="interval"/>
     <ol>
       <li v-for="(group,index) in reslut" :key="index">
-        <h3 class="title">
-          <span>{{ beautify(group.title) }}</span>
-          <span>￥{{ group.total }}</span>
-        </h3>
+        <h3>{{ group.year }}</h3>
         <ol>
-          <li class="record-item" v-for="(item,index) in group.items" :key="index">
-            <span>{{ tagString(item.tags) }}</span>
-            <span class="notes">{{ item.note }}</span>
-            <span>￥{{ item.account }}</span>
+          <li v-for="(groupItem,index) in group.list" :key="index">
+            <h3 class="title">
+              <span>{{ beautify(groupItem.title) }}</span>
+              <span>￥{{ groupItem.total }}</span>
+            </h3>
+            <ol>
+              <li class="record-item" v-for="(item,index) in groupItem.items" :key="index">
+                <span>{{ tagString(item.tags) }}</span>
+                <span class="notes">{{ item.note }}</span>
+                <span>￥{{ item.account }}</span>
+              </li>
+            </ol>
           </li>
         </ol>
       </li>
