fa5a50984c6e7621330037409cab1ebeb5cb0efd
myself <1907312518@qq.com>
2023-03-02 12:19:39 +0800

    保证用户用手机打开页面，否则提示用户

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,17 @@ const app = createApp(App);
 app.component('Layout', Layout);
 app.component('Icon', Icon);
 app.use(store).use(router).mount('#app');
+
+
+if (document.documentElement.clientWidth > 500) {
+  window.alert('请手机打开页面，以保证浏览效果');
+  const imgItem = document.createElement('img');
+  imgItem.src = '/qrcode.png';
+  document.body.appendChild(imgItem);
+  imgItem.style.width = '20%';
+  imgItem.style.position = 'fixed';
+  imgItem.style.left = '50%';
+  imgItem.style.top = '50%';
+  imgItem.style.transform = 'translate(-50%,-50%)';
+  imgItem.style.boxShadow = '0 0 10px rgba(0,0,0,.25)';
+}
