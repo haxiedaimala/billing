cbba10b901fce7ceaffa38b24f192bb72bcd6c6f
myself <1907312518@qq.com>
2023-03-02 16:45:40 +0800

    去除部署 billing-website 后，二维码地址不正确的问题

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+import {ref} from 'vue';
+
+const qrcodeImg = ref<HTMLImageElement>();
+const visible = ref(false);
+if (document.documentElement.clientWidth > 500) {
+  window.alert('请手机打开页面，以保证浏览效果');
+  visible.value = true;
+}
+</script>
 <template>
   <router-view/>
+  <Teleport to="body">
+    <img ref="qrcodeImg" src="./assets/qrcode.png" alt="qrcode" class="qrcode" v-if="visible">
+  </Teleport>
 </template>
 
 <style lang="scss">
@@ -14,5 +27,14 @@
   -moz-osx-font-smoothing: grayscale;
   color: #333;
   background: #f5f5f5;
+}
+
+.qrcode {
+  width: 20%;
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  box-shadow: 0 0 10px rgba(0, 0, 0, .25);
 }
 </style>
