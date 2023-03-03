4236fe516eff8941f735eda3246e641a7d4d6c63
myself <1907312518@qq.com>
2023-03-03 21:22:40 +0800

    完善 echarts 样式

--- src/components/ECharts.vue
+++ src/components/ECharts.vue
@@ -2,7 +2,6 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-ignore
 import * as echarts from 'echarts';
-
 import {onMounted, ref} from 'vue';
 
 type EChartsOption = echarts.EChartsOption;
@@ -22,7 +21,16 @@ onMounted(() => {
         '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
         '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
         '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
-      ]
+      ],
+      axisTick: {
+        show: true,
+        alignWithLabel: true
+      },
+      axisLine: {
+        lineStyle: {
+          color: '#666'
+        }
+      }
     },
     yAxis: {
       type: 'value',
@@ -30,16 +38,31 @@ onMounted(() => {
     },
     series: [
       {
+        symbolSize: 12,
+        symbol: 'circle',
         data: [
           150, 230, 224, 218, 135, 147, 260, 25, 36, 36,
           150, 230, 224, 218, 135, 147, 260, 25, 36, 36,
           150, 230, 224, 218, 135, 147, 260, 25, 36, 36,
         ],
-        type: 'line'
+        type: 'line',
+        itemStyle: {
+          color: '#666'
+        }
       }
     ],
     tooltip: {
-      show: true
+      show: true,
+      triggerOn: 'click',
+      confine: true,
+      trigger: 'item',
+      position: 'top',
+      formatter: '{c}',
+      backgroundColor: '#666',
+      textStyle: {
+        color: '#fff'
+      },
+      extraCssText: 'box-shadow: 0 0 3px rgba(0, 0, 0, 0.3);'
     }
   };
   option && myChart.setOption(option);
